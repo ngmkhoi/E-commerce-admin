af2bdc5b41e00f71d0b6e8dd8f4680ee4e5a2ea9
HiXao <115935880+HiXao@users.noreply.github.com>
2023-11-04 13:14:30 +0700

    hoàn thiện category

--- models/Category.js
+++ models/Category.js
@@ -2,10 +2,19 @@
 import mongoose, { Schema, model,models } from "mongoose"
 
 const CategorySchema = new Schema({
-    name: {type: String, required: true},
-    parent: {type:mongoose.Types.ObjectId, ref:'Category'},
-    properties: [{type:Oject}]
-}); 
+    name: {
+        type: String,
+        required: true,
+        // Thêm ràng buộc hoặc tùy chọn khác ở đây
+    },
+    parent: {
+        type: mongoose.Types.ObjectId,
+        ref: 'Category',
+        // Thêm ràng buộc hoặc tùy chọn khác ở đây
+    },
+    properties: [{ type: Object }],
+    // Thêm các thuộc tính khác ở đây
+});
 
 export const Category = models?.Category || model('Category', 
 CategorySchema);
